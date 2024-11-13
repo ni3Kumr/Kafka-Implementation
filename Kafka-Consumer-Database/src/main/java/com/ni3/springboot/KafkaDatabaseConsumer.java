ac3d184727bf25a668cfcea54b9be7ef0f86915e
Nitin Kumar <121708351+ni3Kumr@users.noreply.github.com>
2024-11-13 07:42:04 +0530

    Adding Consumer and database configuration

--- Kafka-Consumer-Database/src/main/java/com/ni3/springboot/KafkaDatabaseConsumer.java
+++ Kafka-Consumer-Database/src/main/java/com/ni3/springboot/KafkaDatabaseConsumer.java
@@ -4,6 +4,7 @@ import com.ni3.springboot.entity.WikimediaData;
 import com.ni3.springboot.repository.WikiMediaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,8 @@ public class KafkaDatabaseConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
     @KafkaListener(
-            topics = "wikimedia_recentchange",
-            groupId = "myGroup"
+            topics ="${spring.kafka.topic.name}" ,
+            groupId = "${spring.kafka.consumer.group-id}"
     )
     public void consume(String eventMessage){
 
